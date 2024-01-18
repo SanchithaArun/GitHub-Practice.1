113823a472b2946e6299dd708707147ffdc811f3
asanc <a.sanchitha@gmail.com>
2024-01-17 21:19:45 -0600

    Demo Commit and Push

--- src/test/java/gitHubOperation/Sample1.java
+++ src/test/java/gitHubOperation/Sample1.java
@@ -1,11 +1,10 @@
-package githuboperation;
+package gitHubOperation;
 
 public class Sample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to GitHub");
-
+        System.out.println("Hi!!My 1st Github program");
 	}
 
 }
